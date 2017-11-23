5a1a762bb44e1a7cce78d72abdedc761fa5accd6
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-11-23 01:45:37 -0500

    Update User factory to use 25 char max usernames

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
-    sequence(:username) { |n| "#{Faker::Internet.user_name}#{n}" }
+    sequence(:username) { |n| "#{Faker::Internet.user_name(8..25)}#{n}" }
 
     trait :confirmed do
       confirmed_at Time.now
